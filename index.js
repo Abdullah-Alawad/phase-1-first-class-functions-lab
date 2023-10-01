cb65d5efc0daf597283259ea1d3e1d4309b20874
Abdullah Alawad <abdullahalawad123321@gmail.com>
2023-10-01 15:14:22 +0300

    Tests are Done!

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+//1
+const returnFirstTwoDrivers = function(drivers){
+    const newDriver = [];
+    newDriver.push(drivers[0]);
+    newDriver.push(drivers[1]);
+    return newDriver;
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(2, 4);
+}
+
+const selectingDrivers =[returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x){
+    return function(fare){
+        return fare * x;
+    }
+}
+
+function fareDoubler(x){
+    return x * 2;
+}
+
+function fareTripler(x){
+    return x * 3;
+}
+
+function selectDifferentDrivers(drivers, fun1){
+    return fun1(drivers);
+}
